54efa02f0081a719004b8818f26c7f3d272d4b5d
NuvaGit <jackneilan02@gmail.com>
2025-05-19 17:32:07 +0100

    gg

---
+++ MollyCounter/MollyCounter/MollyCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MollyCounterApp.swift
+//  MollyCounter
+//
+//  Created by Jack Neilan on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MollyCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MollyCounter/MollyCounterTests/MollyCounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  MollyCounterTests.swift
+//  MollyCounterTests
+//
+//  Created by Jack Neilan on 19/05/2025.
+//
+
+import Testing
+
+struct MollyCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
